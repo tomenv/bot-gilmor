043f4c20ba7ebba5da22f9364f2b6c5e05795850
tomenv <96696078+tomenv@users.noreply.github.com>
2024-12-07 22:43:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ load_dotenv()
 # Configure Streamlit page settings
 st.set_page_config(
     page_title="Chat with Gemini",
-    page_icon=":brain:",  # Favicon emoji
+    page_icon=":banana:",  # Favicon emoji
     layout="centered",  # Page layout option
 )
 
